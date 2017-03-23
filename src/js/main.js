acac59139b9befa1772fff2d1504555a8f61ed2e
emma <emmaruthoneill@gmail.com>
2017-03-22 17:22:18 -0700

    put in colored bar for kurtis and fixed size of map

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,6 @@ var lightest_gray = "#D8D8D8";
 // helpful functions:
 var formatthousands = d3.format("0,000");
 
-// color counties on map
 // function for shading colors
 function shadeColor2(color, percent) {
     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
@@ -20,9 +19,11 @@ var updateInfo = function(year) {
   document.querySelector(".info").innerHTML = `<strong>${year}</strong>`;
 };
 
+// we are not using a projection for the map
 var path = d3.geo.path()
   .projection(null);
-  // show tooltip
+
+// show tooltip
 var tooltip = d3.select("#county-map")
   .append("div")
   .attr("class","tooltip")
@@ -34,6 +35,7 @@ var tooltip = d3.select("#county-map")
 // var mapname = "./assets/maps/ca_county_insets.json";
 var mapname = "./assets/maps/bacounties_topo.json";
 
+// defining the svg that we are using for the map
 var svg = d3.select("#county-map")
   .append("div")
   .classed("svg-container", true) //container class to make it responsive
@@ -41,28 +43,30 @@ var svg = d3.select("#county-map")
   .append("svg")
   //responsive SVG needs these 2 attributes and no width and height attr
   .attr("preserveAspectRatio", "xMinYMin slice")
-  .attr("viewBox", "0 0 860 1000")
+  .attr("viewBox", "0 0 960 1000")
   //class to make it responsive
   .classed("svg-content-responsive", true)
 
-// d3.selectAll(".camapinset").classed("active",false);
+// initialize the map
 updateInfo("2011");
 drawmap_initial(mapname,census2011);
 
+// initialize the years
 var years = [2011, 2012, 2013, 2014, 2015, 2016];
 var i = 0;
 
+// start looping through the years
 var loop = null;
 var tickTime = function() {
   drawmap(mapname,eval("census"+years[i]));
   updateInfo(years[i]);
   i = (i + 1) % years.length;
   loop = setTimeout(tickTime, i == 0 ? 2000 : 2000);
-  // loop = setTimeout(tick, i == 0 ? 1700 : 1000);
 };
-
 tickTime();
 var looping = true;
+
+// event listener for the drop down
 var dropdown = document.querySelector("select");
 // if user picks the year, we update the selected mode and stop looping
 dropdown.addEventListener("change", function() {
@@ -74,6 +78,7 @@ dropdown.addEventListener("change", function() {
   updateInfo(dropdown.value);
 });
 
+// event listener for the play button
 document.querySelector(".start").addEventListener("click", function(e) {
   if (looping) { return }
   document.querySelector(".start").classList.add("selected");
@@ -82,6 +87,7 @@ document.querySelector(".start").addEventListener("click", function(e) {
   dropdown.value = "--";
   tickTime();
 })
+// event listener for the pause button
 document.querySelector(".pause").addEventListener("click", function(e) {
   if (!looping) { return }
   document.querySelector(".start").classList.remove("selected");
@@ -90,7 +96,7 @@ document.querySelector(".pause").addEventListener("click", function(e) {
   clearTimeout(loop);
 })
 
-// generate map
+// update map with new data
 function drawmap(active_map,active_data) {
 
     d3.json(active_map, function(error, us) {
@@ -103,11 +109,8 @@ function drawmap(active_map,active_data) {
         // .ease("linear")
         // .duration(500)
         .style("fill", function(d) {
-          // var location = Number(d.id);
           var location = d.id;
-          // console.log(active_data["county_name"]);
           if (active_data[location]) {
-            // var mig = active_data[location]["migration"]/22000;
             var mig = 1-Math.abs(active_data[location]["migration_percent"]/1.15);
             if (active_data[location]["migration_percent"] > 0) {
               return shadeColor2("#265B9B", mig)
@@ -123,7 +126,7 @@ function drawmap(active_map,active_data) {
         .on('mouseover', function(d,index) {
           var location = d.id;
           if (active_data[location]) {
-            var html_str = "<div class='name'>"+location+" County</div><div>Total migration: "+formatthousands(active_data[location]["migration"])+"</div><div>Total population: "+formatthousands(active_data[location]["population"])+"</div><div>Percentage of population migrating: "+formatthousands(active_data[location]["migration_percent"])+"%</div>";
+            var html_str = "<div class='name'>"+location+" County</div><div>Net migration: "+formatthousands(active_data[location]["migration"])+"</div><div>Total population: "+formatthousands(active_data[location]["population"])+"</div><div>Percentage of population migrating: "+formatthousands(active_data[location]["migration_percent"])+"%</div>";
             tooltip.html(html_str);
             tooltip.style("visibility", "visible");
           }
@@ -146,40 +149,16 @@ function drawmap(active_map,active_data) {
         .on("mouseout", function(){
           return tooltip.style("visibility", "hidden");
         });
-
-      // var exiting = features.exit()
-      //   .transition()
-      //   .duration(1000);
-      //
-      // console.log(exiting);
-      // exiting.remove();
   });
 
 };
 
-// generate map
+// initialize map
 function drawmap_initial(active_map,active_data) {
-
-    // d3.select("#county-map").select("svg").transition().duration(10).remove();
-    // d3.select("#county-map").select(".svg-container").transition().duration(10).remove();
-    //
-    // CA map by county
-    // var svg = d3.select("#county-map")
-    //   .append("div")
-    //   .classed("svg-container", true) //container class to make it responsive
-    //   .attr("id","county-map")
-    //   .append("svg")
-    //   //responsive SVG needs these 2 attributes and no width and height attr
-    //   .attr("preserveAspectRatio", "xMinYMin slice")
-    //   .attr("viewBox", "0 0 860 1000")
-    //   //class to make it responsive
-    //   .classed("svg-content-responsive", true)
-      // .append("g")
-      //   .attr("transform", "translate(" + 100 + "," + 0 +") scale(0.5, 0.5)");
 
     d3.json(active_map, function(error, us) {
       if (error) throw error;
-    //
+
       var features = topojson.feature(us,us.objects.features).features;
       var nodes = svg.selectAll(".states")
         .data(topojson.feature(us, us.objects.features).features);
@@ -188,14 +167,9 @@ function drawmap_initial(active_map,active_data) {
         .attr("class", "states")
         .attr("d",path)
         .style("fill", function(d) {
-          console.log(d);
-          // var location = Number(d.id);
           var location = d.id;
-          // console.log(active_data["county_name"]);
           if (active_data[location]) {
-            // var mig = active_data[location]["migration"]/22000;
             var mig = 1-Math.abs(active_data[location]["migration_percent"]/1.15);
-            console.log(mig);
             if (active_data[location]["migration_percent"] > 0) {
               return shadeColor2("#265B9B", mig)
             } else {
